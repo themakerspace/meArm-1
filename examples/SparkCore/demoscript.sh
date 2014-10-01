3b988808a698affc59aabb54c339d3d9d6987145
paulscott56 <pscott209@gmail.com>
2014-10-01 12:20:25 +0200

    Demo script to send POST requests via cURL to meArm via the Spark build API

---
+++ examples/SparkCore/demoscript.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+curl https://api.spark.io/v1/devices/55ff68065075555357271887/move -d access_token=872c3ebc10fe1759abf5749607c9cd0cb8083d67 -d args=movefwd,90
+curl https://api.spark.io/v1/devices/55ff68065075555357271887/move -d access_token=872c3ebc10fe1759abf5749607c9cd0cb8083d67 -d args=moveup,120
+curl https://api.spark.io/v1/devices/55ff68065075555357271887/move -d access_token=872c3ebc10fe1759abf5749607c9cd0cb8083d67 -d args=claw,0
+curl https://api.spark.io/v1/devices/55ff68065075555357271887/move -d access_token=872c3ebc10fe1759abf5749607c9cd0cb8083d67 -d args=claw,40
+curl https://api.spark.io/v1/devices/55ff68065075555357271887/move -d access_token=872c3ebc10fe1759abf5749607c9cd0cb8083d67 -d args=rotate,0
+curl https://api.spark.io/v1/devices/55ff68065075555357271887/move -d access_token=872c3ebc10fe1759abf5749607c9cd0cb8083d67 -d args=movefwd,130
+curl https://api.spark.io/v1/devices/55ff68065075555357271887/move -d access_token=872c3ebc10fe1759abf5749607c9cd0cb8083d67 -d args=claw,0
+curl https://api.spark.io/v1/devices/55ff68065075555357271887/move -d access_token=872c3ebc10fe1759abf5749607c9cd0cb8083d67 -d args=moveup,90
+curl https://api.spark.io/v1/devices/55ff68065075555357271887/move -d access_token=872c3ebc10fe1759abf5749607c9cd0cb8083d67 -d args=rotate,90
